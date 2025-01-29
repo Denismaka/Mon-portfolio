2f86674e6f11e45253ec56767e6697b7638703d8
Denismaka <makadenis370@gmail.com>
2025-01-29 22:23:02 +0100

    correction du bug au niveau du footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,38 +1,36 @@
-
-
-
-  <!-- ############## -->
-    <!-- Footer -->
-    <!-- ############## -->
+<!-- ############## -->
+<!-- Footer -->
+<!-- ############## -->
 <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-2">
-            <a href="#"
-              ><h4>denis <span>maka</span></h4></a
-            >
-            <p>développeur front-end</p>
-          </div>
-          <div class="col-md-6">
-            <div class="socials-media">
-              <a href="#"><i class="fab fa-facebook"></i></a>
-              <a href="#"><i class="fab fa-github"></i></a>
-              <a href="#"><i class="fab fa-x-twitter"></i></a>
-              <a href="#"><i class="fab fa-linkedin"></i></a>
-              <a href="#"><i class="fab fa-discord"></i></a>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <p>Copyright &copy;2024 -denis maka- Tous droits réservés</p>
-          </div>
+  <div class="container" style="padding: 2rem 0 5rem 0;">
+    <div class="row">
+      <div class="col-md-2">
+        <a href="#">
+          <h4>denis <span>maka</span></h4>
+        </a>
+        <p>développeur front-end</p>
+      </div>
+      <div class="col-md-6">
+        <div class="socials-media">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-github"></i></a>
+          <a href="#"><i class="fab fa-x-twitter"></i></a>
+          <a href="#"><i class="fab fa-linkedin"></i></a>
+          <a href="#"><i class="fab fa-discord"></i></a>
         </div>
       </div>
-    </footer>
+      <div class="col-md-4">
+        <p>Copyright &copy;2024 -denis maka- Tous droits réservés</p>
+      </div>
+    </div>
+  </div>
+</footer>
 
 
-    <script src="assets/typed.js/dist/typed.umd.js"></script>
-    <script src="assets/scrollreveal/dist/scrollreveal.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/app.js"></script>
+<script src="assets/typed.js/dist/typed.umd.js"></script>
+<script src="assets/scrollreveal/dist/scrollreveal.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/app.js"></script>
 </body>
+
 </html>
